80deab57719771fecf5e42831c9b18c6f38b356f
cygrove <wanlong2713@163.com>
2019-07-03 13:55:11 +0800

    commit

--- app/src/main/java/com/cygrove/statusbar/MainActivity.java
+++ app/src/main/java/com/cygrove/statusbar/MainActivity.java
@@ -2,8 +2,7 @@ package com.cygrove.statusbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import statusbar.cygrove.com.statusbarhelper.StatusBarHelper;
+import statusbar.cygrove.com.statusbarhelper.immersionbar.ImmersionBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StatusBarHelper.darkMode(this);
+        ImmersionBar.with(this).fitsSystemWindows(false).transparentStatusBar().init();
+    }
+
+    /**
+     * 初始化沉浸式
+     * Init immersion bar.
+     */
+    protected void initImmersionBar() {
+        //设置共同沉浸式样式
+        ImmersionBar.with(this).navigationBarColor(R.color.colorPrimary).init();
     }
 }
