6d8576cdb0174fbaa29d18e9595e59536aae7b57
cygrove <wanlong2713@163.com>
2019-07-03 10:42:47 +0800

    commit

---
+++ statusbarhelper/src/main/java/statusbar/cygrove/com/statusbarhelper/immersionbar/ImmersionCallback.java
@@ -0,0 +1,10 @@
+package statusbar.cygrove.com.statusbarhelper.immersionbar;
+
+/**
+ * The interface Immersion callback.
+ *
+ * @author geyifeng
+ * @date 2019 /4/11 5:04 PM
+ */
+interface ImmersionCallback extends OnNavigationBarListener, Runnable {
+}
